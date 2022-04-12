5a0130a5cf7f1e30012d7e3184800af588b9e760
justinaaa <b26539611@gmail.com>
2022-04-13 00:46:56 +0800

    final

--- 0323 YouTube/playList.js
+++ 0323 YouTube/playList.js
@@ -2,11 +2,19 @@
 let playList = [
     "S7dzAVUerV4",
     "QvYM-iIHDl0",
-    "Zn8M6mcGNoo"
+    "Zn8M6mcGNoo",
+    "boETEUNxZJY",
+    "3u-74-tUtBU",
+    "3kztaCUhhr8",
+    "vyklSbH58u0"
     ];
     //播放起訖秒數
 let playTime = [
     [148,169],
     [66,105],
-    [102,145]
+    [102,145],
+    [48,85],
+    [14,57],
+    [2,35],
+    [65,90]
 ];
