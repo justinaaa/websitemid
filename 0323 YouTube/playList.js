7096dbfbe31ffc90c867a6b493c8ebe6893fa760
justinaaa <b26539611@gmail.com>
2022-04-12 17:18:16 +0800

    changemusic

--- 0323 YouTube/playList.js
+++ 0323 YouTube/playList.js
@@ -1,12 +1,12 @@
 //YouTube Video ID
 let playList = [
-    "SwZMS-ISbH4",
+    "S7dzAVUerV4",
     "QvYM-iIHDl0",
     "Zn8M6mcGNoo"
     ];
     //播放起訖秒數
 let playTime = [
-    [101,109],
-    [144,150],
-    [146,152]
+    [148,167],
+    [66,100],
+    [102,142]
 ];
